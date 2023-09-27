0ee6d72486b82b74d776cbf7cff963c91c57a290
Minhchau <holatuwol@gmail.com>
2023-09-28 01:00:52 +0900

    Allow a single aikurumi entry to be split across multiple teams

--- aikurumi.py
+++ aikurumi.py
@@ -9,13 +9,27 @@ units = {unit_id: unit_name for unit_id, unit_name in sorted(zip(units_df['unitI
 def get_link(video):
 	return '%s - %s' % (video['name'], video['link'])
 
-def get_timing(team):
-	if len(team['links']) == 0:
-		return 'full auto' if team['autoDamage'] is not None else 'semi auto' if team['halfAutoDamage'] is not None else 'unspecified'
+def extract_team(team, full_boss_id, damage_type):
+	if damage_type not in team or team[damage_type] is None:
+		return None
 
-	return 'video'
+	team_data = {
+		'boss': full_boss_id,
+		'region': 'JP',
+		'timing': 'full auto' if damage_type == 'autoDamage' else 'semi auto' if damage_type == 'halfAutoDamage' else 'video' if len(team['links']) != 0 else 'unspecified',
+		'timeline': 'aikurumi %s %s' % (team['id'], '' if len(team['links']) != 1 else team['links'][0]['link']),
+		'damage': '%sw' % team[damage_type]
+	}
 
-def extract_team(boss, team):
+	for i, unit in enumerate(team['charas']):
+		team_data['slot%d' % (5 - i)] = units[int(unit['prefabId'] / 100)]
+		team_data['build%d' % (5 - i)] = '%d⭐; R%d' % (unit['rarity'], unit['rank'])
+
+	team_data['notes'] = ('%s %s' % (team['exRemarks'].strip(), '\n'.join([get_link(video) for video in team['links']]).strip())).strip()
+
+	return team_data
+
+def extract_teams(boss, team):
 	if team['stage'] < 3:
 		tier = 'B'
 	elif team['stage'] < 4:
@@ -31,21 +45,31 @@ def extract_team(boss, team):
 	full_boss_id = '%s%s' % (tier, boss_index)
 	missing_unit = False
 
-	team_data = {
-		'boss': full_boss_id,
-		'region': 'JP',
-		'timing': get_timing(team),
-		'timeline': 'aikurumi %s %s' % (team['id'], '' if len(team['links']) != 1 else team['links'][0]['link']),
-		'damage': '%sw' % [damage for damage in [team['autoDamage'], team['halfAutoDamage'], team['easyManualDamage'], team['damage']] if damage is not None][0]
-	}
+	new_teams = []
 
-	for i, unit in enumerate(team['charas']):
-		team_data['slot%d' % (5 - i)] = units[int(unit['prefabId'] / 100)]
-		team_data['build%d' % (5 - i)] = '%d⭐; R%d' % (unit['rarity'], unit['rank'])
+	if 'autoDamage' in team and team['autoDamage'] is not None:
+		new_team = extract_team(team, full_boss_id, 'autoDamage')
+		if new_team is not None:
+			new_teams.append(new_team)
 
-	team_data['notes'] = ('%s %s' % (team['exRemarks'].strip(), '\n'.join([get_link(video) for video in team['links']]).strip())).strip()
+	if 'halfAutoDamage' in team and team['halfAutoDamage'] is not None and team['halfAutoDamage'] != team['autoDamage']:
+		new_team = extract_team(team, full_boss_id, 'halfAutoDamage')
+		if new_team is not None:
+			new_teams.append(new_team)
 
-	return team_data
+	if 'easyManualDamage' in team and team['easyManualDamage'] is not None and team['easyManualDamage'] != team['halfAutoDamage']:
+		new_team = extract_team(team, full_boss_id, 'easyManualDamage')
+		if new_team is not None:
+			new_teams.append(new_team)
+
+	has_damage = sum([1 if team[damage_type] == team['damage'] else 0 for damage_type in ['easyManualDamage', 'halfAutoDamage', 'autoDamage']]) > 0
+
+	if not has_damage:
+		new_team = extract_team(team, full_boss_id, 'damage')
+		if new_team is not None:
+			new_teams.append(new_team)
+
+	return new_teams
 
 teams = []
 
@@ -56,6 +80,8 @@ for file in os.listdir('aikurumi'):
 	with open('aikurumi/%s' % file, 'r') as f:
 		data = json.load(f)
 
-	teams = teams + [extract_team(boss, team) for boss in data['data'] for team in boss['tasks']]
+	for boss in data['data']:
+		for team in boss['tasks']:
+			teams = teams + list(extract_teams(boss, team))
 
 pd.DataFrame([team for team in teams if team is not None]).to_csv('aikurumi/aikurumi.csv', sep='\t', index=False)
