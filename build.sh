bd7bc3ee368dfae517f9abf697f6e53b037ca768
Minhchau <holatuwol@gmail.com>
2024-02-24 12:32:35 +0900

    Add arguments to build and unit data pull script

--- build.sh
+++ build.sh
@@ -3,16 +3,19 @@
 npm install
 python unit_data.py
 
-node node_modules/typescript/bin/tsc --build tsconfig-alliance.json
-node node_modules/typescript/bin/tsc --build tsconfig-allocate.json
-node node_modules/typescript/bin/tsc --build tsconfig-boxcheck.json
-node node_modules/typescript/bin/tsc --build tsconfig-bosses.json
-node node_modules/typescript/bin/tsc --build tsconfig-clanhits.json
-node node_modules/typescript/bin/tsc --build tsconfig-happening.json
-node node_modules/typescript/bin/tsc --build tsconfig-levelcap.json
-node node_modules/typescript/bin/tsc --build tsconfig-stamina.json
-node node_modules/typescript/bin/tsc --build tsconfig-teams.json
-node node_modules/typescript/bin/tsc --build tsconfig-units.json
-
-cp static/*.html build/
-cp static/*.css build/
+if [ "all" == "${1}" ]; then
+	node node_modules/typescript/bin/tsc --build tsconfig-alliance.json
+	node node_modules/typescript/bin/tsc --build tsconfig-allocate.json
+	node node_modules/typescript/bin/tsc --build tsconfig-boxcheck.json
+	node node_modules/typescript/bin/tsc --build tsconfig-bosses.json
+	node node_modules/typescript/bin/tsc --build tsconfig-clanhits.json
+	node node_modules/typescript/bin/tsc --build tsconfig-happening.json
+	node node_modules/typescript/bin/tsc --build tsconfig-levelcap.json
+	node node_modules/typescript/bin/tsc --build tsconfig-stamina.json
+	node node_modules/typescript/bin/tsc --build tsconfig-teams.json
+	node node_modules/typescript/bin/tsc --build tsconfig-units.json
+else
+	node node_modules/typescript/bin/tsc --build tsconfig-bosses.json
+	node node_modules/typescript/bin/tsc --build tsconfig-teams.json
+	node node_modules/typescript/bin/tsc --build tsconfig-units.json
+fi
--- unit_data.py
+++ unit_data.py
@@ -1,7 +1,13 @@
 import json
 import pandas as pd
+import sys
 
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQNZv9nmkVrGAaih2c4NyYfSiwmB3CC5mmicG_NOvb1kuXrdVUyCmW1q1DWIcCqXgtwPlYK_inNO3VV/pub?output=csv')
+if len(sys.argv) > 2 and sys.argv[1] == 'pull':
+	df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQNZv9nmkVrGAaih2c4NyYfSiwmB3CC5mmicG_NOvb1kuXrdVUyCmW1q1DWIcCqXgtwPlYK_inNO3VV/pub?output=csv')
+
+	df.to_csv('unit_data.csv', index=False)
+else:
+	df = pd.read_csv('unit_data.csv')
 
 units = {unit_id: unit_name for unit_id, unit_name in sorted(zip(df['unitId'], df['name']), key=lambda x: x[1])}
 alt_units = {unit_id: unit_name for unit_id, unit_name in sorted(zip(df['unitId'], df['name.short']), key=lambda x: x[1])}
