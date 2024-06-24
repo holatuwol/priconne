a029f95ca193ee1392e72646c007714af5ce5919
Minhchau <holatuwol@gmail.com>
2024-06-23 17:09:04 -0700

    Updates for CB76

--- src/teams/globalvars.ts
+++ src/teams/globalvars.ts
@@ -1,4 +1,4 @@
-var latestCBId = '75';
+var latestCBId = '76';
 
 var noticeElement = document.getElementById('planner-notice');
 var currentCBId = noticeElement ? noticeElement.getAttribute('data-cb-id') || latestCBId : latestCBId;
