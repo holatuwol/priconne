c9f3f172ebbcb3d305e3412e6c03689a7be5b5c9
Minhchau <holatuwol@gmail.com>
2024-09-24 22:22:29 +0900

    Improve checking for units and latest 6* units

--- unit_image.sh
+++ unit_image.sh
@@ -19,13 +19,15 @@ for id in $(grep bossIds src/bosses/boss_data.ts | grep -o '[0-9]*' | sort -u); 
   get_image ${id}
 done
 
-for id in $(grep -o '"[0-9]*"' src/units/unit_data.ts | sort -u | cut -d'"' -f 2); do
+for id in $(grep -o '\["[0-9]*"' src/units/unit_data.ts | sort -u | cut -d'"' -f 2); do
   for stars in 1 3; do
     get_image ${id}${stars}1
   done
 done
 
-for name in $(grep hasSixStar static/cb76.html | grep -o "'[^']*'" | cut -d"'" -f 2); do
+latest_cb=$(find static -name 'cb*.html' | sed 's|^static/cb||g' | sed 's|.html$||g' | sort -n | tail -1)
+
+for name in $(grep hasSixStar static/cb${latest_cb}.html | grep -o "'[^']*'" | cut -d"'" -f 2); do
   id=$(grep ",${name}," unit_data.csv | cut -d',' -f 1)
   if [ "" != "${id}" ]; then
     get_image ${id}61
