5b7f03a277280f70cb39cfc96e6170d585a84563
Minhchau <holatuwol@gmail.com>
2024-09-24 22:19:08 +0900

    Use a fixed version of the nginx image

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker run --name temp-web-server --rm -p 9080:80 \
 	$(for file in $(find build -maxdepth 1); do echo "-v ./${file}:/usr/share/nginx/html/$(basename ${file})"; done) \
 	$(for file in $(find static -maxdepth 1); do echo "-v ./${file}:/usr/share/nginx/html/$(basename ${file})"; done) \
-	nginx:latest
+	nginx:1.27.1-alpine3.20-perl
